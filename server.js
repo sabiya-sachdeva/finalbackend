2ec6c7e7c29fcd11def590cb7ca42a22836dfd5e
Sabiya <sabiya.sachdeva@gmail.com>
2023-08-10 15:21:06 -0700

    first commit

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ const bookRouter=require('./routes/book')
 
 
 //const uri="mongodb://127.0.0.1:27017/BookList";
-const uri="mongodb+srv://user:123@cluster0.24chgkl.mongodb.net/?retryWrites=true&w=majority"
+const uri="mongodb+srv://mylibrary:123@bookinfo.qi1yw1g.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(uri,{
-    useNewUrlParser:true
+    useNewUrlParser:true,
+    useUnifiedTopology: true
 })
 const connection=mongoose.connection;
 connection.once("open",()=>{
     console.log("database connected");
 })
+
 
 const app=express();
 app.use(cors());
